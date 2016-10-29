259602c69e7146f11e9e73cf19490e9159a75f69
mocayo <qihaiqh@qq.com>
2016-10-29 12:14:50 +0800

    10-29

--- lad.py
+++ lad.py
@@ -20,7 +20,8 @@ pl.style.use('ggplot')
 
 def fun(x1, x2, p):
 	theta1, theta2, theta3 = p
-	return theta1 + theta2*np.sin(x1) + theta3*np.exp(x2)
+	# return theta1 + theta2*np.sin(x1) + theta3*np.exp(x2)
+	return theta1 + theta2*x1 + theta3*x2
 
 preal = np.random.rand(1,3)[0]
 
@@ -32,7 +33,33 @@ ytest = [np.random.normal(0, 0.1) + y for y in yreal]
 
 ptest = np.random.rand(1,3)[0]
 
-pl.plot(yreal, label=u"真实数据")
-pl.plot(ytest, 'o',label=u"带噪声的数据")
-pl.legend()
-pl.show()
+# print ptest
+
+for j in range(3):
+	if j==0:
+		ptest = [0,0,0]
+	print j
+	print ptest
+	# 学习率
+	alpha = 1
+
+	# print fun(x1,x2,ptest)
+	# err = fun(x1,x2,ptest) - yreal
+
+	# t0 = ptest[0] - alpha * sum(err)
+	# t1 = ptest[1] - alpha * sum([err[i] *x1[i] for i in range(len(x1))])
+	# t2 = ptest[2] - alpha * sum([err[i] *x2[i] for i in range(len(x2))])
+	delta = [0,0,0]
+	for i in range(len(x1)):
+		delta[0] = delta[0] + yreal[i] - alpha * fun(x1[i], x2[i], ptest)
+		delta[1] = delta[1] + (yreal[i] - alpha * fun(x1[i], x2[i], ptest)) * x1[i]
+		delta[2] = delta[2] + (yreal[i] - alpha * fun(x1[i], x2[i], ptest)) * x2[i]
+
+	ptmp = [ptest[i] + delta[i] for i in range(len(ptest))]
+	ptest = ptmp
+
+
+# pl.plot(yreal, label=u"真实数据")
+# pl.plot(ytest, 'o',label=u"带噪声的数据")
+# pl.legend()
+# pl.show()
